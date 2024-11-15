a30238033ecab8ac4c8894b7f128bba4d43f6198
joão vitor <joaovitorvalenca92@gmail.com>
2024-11-15 12:32:03 -0300

    telegram

--- app.py
+++ app.py
@@ -3,6 +3,17 @@ from bs4 import BeautifulSoup
 import time
 import pandas  as pd
 import sqlite3
+import asyncio
+from telegram import Bot
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Configurações do bot do Telegram
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+bot = Bot(token=TOKEN)
 
 def fetch_page():
     url = "https://www.mercadolivre.com.br/apple-iphone-16-pro-max-1-tb-titnio-preto-distribuidor-autorizado/p/MLB1040287854#polycard_client=search-nordic&wid=MLB5093482120&sid=search&searchVariation=MLB1040287854&position=8&search_layout=stack&type=product&tracking_id=1c14efda-e073-476c-b8fc-0b889df5a121"
